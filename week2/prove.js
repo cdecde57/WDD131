e3fbdc81a1b8c2e8df51033a85bc9c4d8cef28c7
cdecde57 <cdecde56@gmail.com>
2025-09-19 14:02:44 -0600

    Finished the prove assignment for week 2

---
+++ week2/prove.js
@@ -0,0 +1,38 @@
+// When they select dark theme, apply the dark class and update the image. Remove the class when light theme is used and update the image.
+
+
+// Add an event to the dropdown menu
+var themeSel = document.querySelector("#theme-selector");
+themeSel.addEventListener("change", updateTheme);
+
+
+// A function called when the theme selector drop-down menu is used
+function updateTheme(event){
+
+    // Identify the selected value
+    var selectedTheme = event.target.value;
+
+    // Obtain the elements we'll modify
+    var pageContent = document.querySelector(".page-contents");
+    var pageBody = document.querySelector("body");
+    var byuLogo = document.querySelector("#byui-logo img")
+
+    if (selectedTheme === "dark"){
+
+        // Add the dark class to the application and update the image
+        pageContent.classList.add("dark-text");
+        pageBody.classList.add("dark-background");
+        byuLogo.setAttribute("src","prove-dark.png");
+    }
+
+    else if (selectedTheme === "light"){
+        // remove the dark class to the application and update the image
+        pageContent.classList.remove("dark-text");
+        pageBody.classList.remove("dark-background");
+        byuLogo.setAttribute("src","prove.png");
+    }
+    else{
+        console.log("I guess if I wanted another theme, or a default one I could add one. For now I will just send this message - Hello, World!");
+    }
+
+}
