5f7c8af594e577c081072310ee92df14891ba5bd
cdecde57 <cdecde56@gmail.com>
2025-09-25 20:02:02 -0600

    Week 6 ponder 1 assignment

---
+++ week6/ponder1.js
@@ -0,0 +1,54 @@
+
+// Variables used to create HTML
+const students = [
+    {last: 'Andrus', first: 'Aaron'},
+    {last: 'Masa', first:'Manny'},
+    {last: 'Tanda', first: 'Tamanda'}
+];
+
+const page = document.body;
+
+// No direction was given as to what was wanted for the 'one two three' bullet points, adding it to the HTML here
+page.innerHTML += "<ul><li>one</li><li>two</li><li>three</li></ul>"
+
+function printNames(item){
+    page.innerHTML += `<p style="text-align:center">${item["first"]} ${item["last"]}</p><hr>`;
+}
+
+students.forEach(printNames);
+
+// No idea why the ponder wanted us to add these - it was never specified 
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+function convert(grade) {
+    switch (grade){
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert('not a valid grade');
+    }
+    return points;
+}
+
+
+//// There was no direction in the ponder with where the console output was coming from, why or anything on those lines. As such I created the console output, but the ponder simply doesn't cover anything about what it wants me to do with the JavaScript
+console.log("one");
+console.log("two");
+console.log("three");
+console.log(students);
+console.log("7")
+console.log(`${7 / 3}`)
+console.log(students);
+console.log("2")
