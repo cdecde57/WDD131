9a446a719a04f201d2e8dc66f670757c2a42e38f
cdecde57 <cdecde56@gmail.com>
2025-11-25 13:58:08 -0700

    Fixed some of my comments

--- week12/scripts/index.js
+++ week12/scripts/index.js
@@ -55,7 +55,7 @@ init();
  * My current Pseudo Code 
  * 
 0. Draw upon an EJS module to get default data, sort it, and store it in the 'entry' vector
-1. On page load - populate the budgeter with the 'entry' vector			-/
+1. On page load - populate the budgeter with the 'entry' vector			        -/
 2. Add New Item - Add entry information into the 'entry' vector and add HTML	-|Share an 'add item' function
 3. Remove Last Item - Pop the last item from the vector and re-paint the widget	-\
 4. Analyze - Add income, subtract expenditures. If positive then give a congratz, if negative give a budgeting tip in the 'feedback' row.
