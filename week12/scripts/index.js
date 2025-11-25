58a7b0feae51316ea520edd6d74d415b73b30873
cdecde57 <cdecde56@gmail.com>
2025-11-25 13:57:47 -0700

    Started the JS on the injdex page

--- week12/scripts/index.js
+++ week12/scripts/index.js
@@ -1,7 +1,65 @@
-// Open / close the menu when the 'menu' button is clicked.
-let menuButton = document.querySelector(".header-menu-button");
-let menuBar = document.querySelector(".navigation-bar");
+import budgetWidgetEntries from "./index.mjs";
 
-menuButton.addEventListener("click", ()=>{
+let WIDGET_ENTRIES = [...budgetWidgetEntries];
+
+// Will sort 'a' and 'b' as strings - used in 'paintEntries'
+function sortEntries(a, b) {
+  a = a.entryName.toLowerCase();
+  b = b.entryName.toLowerCase();
+
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function paintHTML(){
+
+}
+
+// Update the HTML on the page to show the budget entries
+function paintEntries() {
+
+  // Start by sorting the entries
+  WIDGET_ENTRIES = WIDGET_ENTRIES.sort(sortEntries);
+
+  // Paint the HTML itself for the entries
+  painHTML();
+
+}
+
+// Initiate general page dynamics
+function init() {
+  // Open / close the menu when the 'menu' button is clicked.
+  let menuButton = document.querySelector(".header-menu-button");
+  let menuBar = document.querySelector(".navigation-bar");
+
+  menuButton.addEventListener("click", () => {
     menuBar.classList.toggle("display-menu");
-});
+  });
+
+  /**
+   * I need to add code to work with the local storage!
+   */
+
+  // Paint the budget widget entries
+  paintEntries();
+}
+
+init();
+
+/**
+ * My current Pseudo Code 
+ * 
+0. Draw upon an EJS module to get default data, sort it, and store it in the 'entry' vector
+1. On page load - populate the budgeter with the 'entry' vector			-/
+2. Add New Item - Add entry information into the 'entry' vector and add HTML	-|Share an 'add item' function
+3. Remove Last Item - Pop the last item from the vector and re-paint the widget	-\
+4. Analyze - Add income, subtract expenditures. If positive then give a congratz, if negative give a budgeting tip in the 'feedback' row.
+
+
+
+ */
---
+++ week12/scripts/index.mjs
@@ -0,0 +1,8 @@
+
+let budgetWidgetEntries = [
+    {entryType : "Income", entryName : "Paycheck", entryAmount : 100},
+    {entryType : "Expenditure", entryName : "Groceries", entryAmount : 13.37},
+    {entryType : "Expenditure", entryName : "Gas", entryAmount : 25.68}
+]
+
+export default budgetWidgetEntries
