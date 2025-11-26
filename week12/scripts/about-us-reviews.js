e9e3b243ccacb8d3d8eb70e4e9c68516eec23128
cdecde57 <cdecde56@gmail.com>
2025-11-26 13:23:11 -0700

    Huge about-us page revision with the featured comment painting

--- week12/scripts/about-us-reviews.js
+++ week12/scripts/about-us-reviews.js
@@ -22,44 +22,79 @@ function updateFeaturedComment(entry) {
   }
 
   // Paint a new featured comment
-  let highlight = document.querySelector(".widget-review-highlight");
+  
+  // Update the image source and alt value
+  let reviewImage = document.querySelector("#review-image");
+  reviewImage.setAttribute("src", image);
+  reviewImage.setAttribute("alt", `Highlight image of ${fullName}`);
 
-  let highlightHTML = `
+  //Update the full name
+  let reviewName = document.querySelector("#widget-full-name");
+  reviewName.textContent = `Name: ${fullName}`;
 
-    <section class="widget-review-slide-section">
-        <button class="widget-reivew-arrow" id="widget-review-left">
-        <img src="./images/left-arrow.png" alt="Left Arrow" />
-        </button>
+  //Update the review
+  let reviewStars = document.querySelector("#widget-reviews");
+  reviewStars.textContent = `Reviews: ${stars}`;
 
-        <div class="widget-review-image">
-        <img src="${image}" alt="Highlight image of ${fullName}" id="review-image"/>
-        </div>
+  //Update the review's comment
+  let reviewComment = document.querySelector(".widget-review");
+  reviewComment.textContent = comment;
 
-        <button class="widget-reivew-arrow" id="widget-review-right">
-        <img src="./images/right-arrow.png" alt="Right Arrow" />
-        </button>
-    </section>
-    <section class="widget-review-details">
-        <p>Name: ${fullName}</p>
-        <p>Reviews: ${stars}</p>
-        <em class="widget-review">
-        ${comment}
-        </em>
-    </section>
+}
 
-    `;
+function evaluateLeft() {
+  // Move the 'current review' index to the left
+  switch (CURRENT_REVIEW) {
+    case 0:
+      CURRENT_REVIEW = 2;
+      break;
+    case 1:
+      CURRENT_REVIEW = 0;
+      break;
+    case 2:
+      CURRENT_REVIEW = 1;
+      break;
+    default:
+      CURRENT_REVIEW = 0;
+  }
 
-  highlight.innerHTML = highlightHTML;
+  // Re-paint the HTML
+  updateFeaturedComment(COMMENTS[CURRENT_REVIEW]);
+  return;
+}
+
+function evaluateRight() {
+  // Move the 'current review' index to the left
+  switch (CURRENT_REVIEW) {
+    case 0:
+      CURRENT_REVIEW = 1;
+      break;
+    case 1:
+      CURRENT_REVIEW = 2;
+      break;
+    case 2:
+      CURRENT_REVIEW = 0;
+      break;
+    default:
+      CURRENT_REVIEW = 0;
+  }
+
+  // Re-paint the HTML
+  updateFeaturedComment(COMMENTS[CURRENT_REVIEW]);
+  return;
 }
 
 function init() {
-
   // Add a random comment into the featured comments section.
-  CURRENT_REVIEW = randomNum(3)
+  CURRENT_REVIEW = randomNum(3);
   updateFeaturedComment(COMMENTS[CURRENT_REVIEW]);
 
   // Setup a listener for when the user uses the right or left arrows so the featured comment changes.
+  let leftArrow = document.querySelector("#widget-review-left");
+  leftArrow.addEventListener("click", evaluateLeft);
 
+  let rightArrow = document.querySelector("#widget-review-right");
+  rightArrow.addEventListener("click", evaluateRight);
 }
 
 init();
@@ -67,7 +102,7 @@ init();
 /**
  * My Current Pseudo Code
  * 0. Import  user comments / information into the comments section and for the featured comments
- * 1. Load a random profile in as the featured comment. Every 8 seconds move 1 to the right (positive index).
+ * 1. Load a random profile in as the featured comment. Every 8 seconds move 1 to the right (positive index). (I don't think I'll add this one, may cause a bad user experience if it collides with their clicking of the reviews)
  * 2. When one of the arrows is pressed, it will go through the list of user comments and render them
  * 3. Make sure to render all of the user comments
  * 4. add a new user comment when someone presses the submit comment button. I'll gather the information similar to the buget tool.
